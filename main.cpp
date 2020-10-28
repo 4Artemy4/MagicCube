9521b1618c3e01a1ab70e91a380bf758080922b2
art <4artemy4>
2020-10-29 01:26:47 +0300

    some documentation

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include "Cube/Cube.h"
 #include "GUI/Drawer.h"
 
+// Press H to horizontally rotation
+// Press V to vertically rotation
+// Press P to front rotation
+// Press enter 
 int main() {
     Cube cube8(8);
     Drawer drawer(cube8);
